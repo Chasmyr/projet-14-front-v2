6efa650181db1c1b9ea6f17383b49b93f4db55c5
Chasmyr <78807306+Chasmyr@users.noreply.github.com>
2023-04-07 13:18:17 +0200

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import CreateEmployee from "./pages/createEmployee"
 import EmployeeList from "./pages/employeeList"
+import ErrorPage from "./pages/error"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<CreateEmployee />} />
         <Route path="/list" element={<EmployeeList />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   )
